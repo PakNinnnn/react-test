2e681607bf2dc43af8bd49f99d3685f57e8867d1
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2024-02-12 01:27:20 +0800

    Added UI for title, searchbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { React, useEffect } from "react";
 import "./App.css";
-import searchIcon from "./search.svg";
+import SearchIcon from "./assets/search.svg";
 
 //API key from OMDBAPI: 4285db42
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=4285db42';
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=4285db42";
+
+const movie1 = {
+    "Title": "Italian Spiderman",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+}
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -18,8 +26,26 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="app">
+      <h1>MovieX</h1>
+
+      <div className="search">
+        <input 
+            type="text" 
+            placeholder="Search for a movie" 
+            value="Spiderman" 
+            onChange={() => {}}
+        /> 
+        <img 
+            src={SearchIcon} 
+            alt="search" 
+            onClick={() => {}}
+        /> 
+      </div>
+
+      <div className="container">
+        
+      </div>
     </div>
   );
 };
