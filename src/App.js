bcb6627a609123db00c52d7bc2a0aa71afe087bb
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2024-02-12 01:02:12 +0800

    Created Index.js and App.js, added API call function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { React, useEffect } from "react";
 
-import './App.css';
+//API key from OMDBAPI: 4285db42
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=4285db42';
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
-    <div className="App">
-      <h1>My React App?!</h1>
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
